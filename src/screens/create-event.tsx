34732b3c3ac6641af803ba614b9a8c70b7d4b1e8
Daryl Browne <daryl.browne@googlemail.com>
2020-02-28 18:12:44 +0000

    refactor(create-event): use collaborator-select component

--- src/screens/create-event.tsx
+++ src/screens/create-event.tsx
@@ -14,6 +14,8 @@ import AuthContext from "../components/auth-context";
 import { humaneAddress, dateAndTimeValidation } from "../utils";
 import DateAndTimeForm from "../components/date-and-time-form";
 import SContainer from "../components/container";
+import { weddingCollaboratorsOrderedByEmailSelector } from "../selectors/selectors";
+import CollaboratorEditableList from "../components/collaborator-editable-list";
 
 const validationSchema = yup.object().shape({
   name: yup.string().required(),
@@ -39,40 +41,7 @@ const CreateEvent: React.FC<IProps> = ({ match, history, mainEvent }) => {
   const { weddingId } = match.params;
   const { user: auth } = useContext(AuthContext);
   const weddingName = useStateSelector(state => state.activeWedding.wedding?.name);
-  const availableCollaborators = useStateSelector(state => state.activeWedding.wedding?._private.collaborators);
-  const [users, setUsers] = useState<{ [id: string]: IUser }>({});
-  const [collaborators, setCollaborators] = useState(new Set<string>());
-  const [showCollabModal, setShowCollabModal] = useState(false);
-
-  useEffect(() => {
-    if (availableCollaborators) {
-      availableCollaborators.forEach(userId => {
-        if (mainEvent) onSelectCollaborator(userId, true);
-
-        db.collection(`users`)
-          .doc(userId)
-          .onSnapshot(snap => {
-            setUsers({ ...users, [snap.id]: { id: snap.id, ...(snap.data() as IUser) } });
-          });
-      });
-    }
-  }, [availableCollaborators]);
-
-  const onSelectCollaborator = (userId: string, value?: boolean) => {
-    const exists = typeof value !== "undefined" ? value : collaborators.has(userId);
-    const updatedCollaborators = new Set(collaborators);
-
-    if (exists) updatedCollaborators.delete(userId);
-    else updatedCollaborators.add(userId);
-
-    setCollaborators(updatedCollaborators);
-  };
-
-  const removeCollaborator = (userId: string) => {
-    const updatedCollaborators = new Set(collaborators);
-    updatedCollaborators.delete(userId);
-    setCollaborators(updatedCollaborators);
-  };
+  const [selectedCollaboratorIds, setSelectedCollaboratorIds] = useState(new Set<string>());
 
   const onSubmit = ({ place, multiService, time, ...values }: TEventFormData) => {
     if (!auth) return;
@@ -86,7 +55,7 @@ const CreateEvent: React.FC<IProps> = ({ match, history, mainEvent }) => {
       _private: { owner: auth.uid },
     };
 
-    if (collaborators.size > 0) eventDetails._private.collaborators = Array.from(collaborators);
+    if (selectedCollaboratorIds.size > 0) eventDetails._private.collaborators = Array.from(selectedCollaboratorIds);
     if (multiService) {
       eventDetails.allowRsvpPerService = true;
       eventDetails.services = {};
@@ -107,7 +76,7 @@ const CreateEvent: React.FC<IProps> = ({ match, history, mainEvent }) => {
     onSubmit,
     enableReinitialize: mainEvent,
   });
-  console.log(formik.errors);
+
   return (
     <SContainer>
       <Heading level={1} children={mainEvent ? "Wedding event details" : "Create event"} />
@@ -176,61 +145,18 @@ const CreateEvent: React.FC<IProps> = ({ match, history, mainEvent }) => {
             ))}
         </Box>
 
-        <Heading level={3} size="small" margin={{ top: "large" }}>
-          Collaborators
-        </Heading>
+        <Heading level={3} size="small" margin={{ top: "large" }} children="Collaborators" />
         <Text>Give your wedding collaborators access to view and edit this event's details, guests and more</Text>
-        <Box direction="row" gap="xsmall" wrap>
-          {Array.from(collaborators).map((userId, idx) => (
-            <Box margin={{ vertical: "small" }} background="light-4" pad="xsmall" direction="row" align="center" alignSelf="start">
-              <Button plain icon={<FormCalendar />} onClick={() => removeCollaborator(userId)} />
-              <Text size="small" color="dark-3" children={users[userId] || userId} />
-            </Box>
-          ))}
+        <Box margin={{ vertical: "medium" }}>
+          <CollaboratorEditableList
+            selectedIds={Array.from(selectedCollaboratorIds)}
+            onSelect={ids => setSelectedCollaboratorIds(new Set(ids))}
+          />
         </Box>
-        <Button margin={{ top: "small" }} label="Add collaborators" onClick={() => setShowCollabModal(true)} />
         <Box margin={{ vertical: "large" }} align="end">
           <Button primary type="submit" label={mainEvent ? "Create wedding event" : "Add event"} />
         </Box>
       </form>
-      {showCollabModal && (
-        <Layer>
-          <Box
-            width={{ max: "600px" }}
-            height={{ min: "300px", max: "600px" }}
-            pad={{ horizontal: "medium", top: "medium", bottom: "xlarge" }}
-          >
-            <Button alignSelf="end" icon={<Close />} onClick={() => setShowCollabModal(false)} />
-            {!availableCollaborators && (
-              <>
-                <Heading level="3" children="No collaborators available" margin={{ top: "none" }} />
-                <Text margin={{ bottom: "medium" }}>
-                  Cannot allow others to edit this event because you have not yet added any collaborators to your wedding.
-                </Text>
-                <Text size="small" color="dark-6" margin={{ bottom: "small" }}>
-                  To give someone else permission to edit this event, you must first give them access to collaborate on your wedding. You
-                  can do that in your <Link to="/weddings">wedding settings</Link>
-                </Text>
-                <Text size="small" color="dark-6">
-                  Once they have accepted your collaboration invite, you can then give them permission to collaborate on this event
-                </Text>
-              </>
-            )}
-            {!!availableCollaborators && (
-              <>
-                <Heading level="3" children="Select collaborator(s)" margin={{ top: "none" }} />
-                {availableCollaborators.map(userId => (
-                  <CheckBox
-                    label={users[userId] || userId}
-                    checked={collaborators.has(userId)}
-                    onChange={() => onSelectCollaborator(userId)}
-                  />
-                ))}
-              </>
-            )}
-          </Box>
-        </Layer>
-      )}
     </SContainer>
   );
 };
