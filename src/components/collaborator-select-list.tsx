697c60f8d0336161fa455e2e295c294ff4a7e8b1
Daryl Browne <daryl.browne@googlemail.com>
2020-02-28 18:13:18 +0000

    refactor(collaborator-select-list): show msg when no collabers available

--- src/components/collaborator-select-list.tsx
+++ src/components/collaborator-select-list.tsx
@@ -23,22 +23,42 @@ const CollaboratorSelectList: React.FC<IProps> = ({ selectedIds, onSelect, onClo
     onSelect(updatedIds);
   };
 
+  const noCollaboratorsMsg = (
+    <>
+      <Heading level={6} as="header" children="No collaborators available" margin={{ top: "none" }} />
+      <Text margin={{ bottom: "medium" }}>
+        Cannot allow others to edit this event because you have not yet added any collaborators to your wedding.
+      </Text>
+      <Text size="small" color="dark-6" margin={{ bottom: "small" }}>
+        To give someone else permission to edit this event, you must first give them access to collaborate on your wedding. You can do that
+        in your wedding settings
+      </Text>
+      <Text size="small" color="dark-6">
+        Once they have accepted your collaboration invite, you can then give them permission to collaborate on this event
+      </Text>
+    </>
+  );
+
   return (
     <Box border={{ side: "horizontal" }} pad={{ bottom: "xsmall" }}>
-      <Heading level={6} as="header">
-        Select Collaborators
-      </Heading>
-      <Box height={{ min: "100px", max: "200px" }} overflow="auto">
-        {collaborators.map(collaborator => (
-          <Box key={collaborator.id} margin={{ vertical: "small" }}>
-            <CheckBox
-              label={collaborator.name || collaborator.email}
-              checked={selectedIds?.includes(collaborator.id)}
-              onChange={onCheck(collaborator.id)}
-            />
+      {!collaborators.length ? (
+        noCollaboratorsMsg
+      ) : (
+        <>
+          <Heading level={6} as="header" children="Select Collaborators" />
+          <Box height={{ min: "100px", max: "200px" }} overflow="auto">
+            {collaborators.map(collaborator => (
+              <Box key={collaborator.id} margin={{ vertical: "small" }}>
+                <CheckBox
+                  label={collaborator.name || collaborator.email}
+                  checked={selectedIds?.includes(collaborator.id)}
+                  onChange={onCheck(collaborator.id)}
+                />
+              </Box>
+            ))}
           </Box>
-        ))}
-      </Box>
+        </>
+      )}
       <Button plain alignSelf="end" onClick={onClose}>
         <Box pad="xsmall" border={{ size: "small" }}>
           <Text size="small" children="Close" />
