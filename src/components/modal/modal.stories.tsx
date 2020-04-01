a3a6701b43e12444e2d7b801e892829765e5f8ee
Daryl Browne <daryl.browne@googlemail.com>
2020-04-02 00:05:12 +0100

    refactor(storybook): update story with modalUI wrapper

--- src/components/modal/modal.stories.tsx
+++ src/components/modal/modal.stories.tsx
@@ -1,29 +1,23 @@
-import React, { ReactNode, useState } from "react";
+import React from "react";
 import Modal from "./modal";
-import { Text, Button } from "grommet";
-import Center from "../../styled-components/storybook-components";
+import { Text } from "grommet";
+import { ModalUiDisplay } from "../../styled-components/storybook-components";
 
 export default {
   title: "Components|Modal",
   component: Modal,
 };
 
-export const ToStorybook = () => {
-  const [showModal, setShowModal] = useState(true);
-  return (
-    <div>
-      <Button onClick={() => setShowModal(true)} label="show/hide modal" />
-      {showModal && (
-        <Modal title="Modal title" onClose={() => setShowModal(false)}>
-          <Text>Modal content</Text>
-        </Modal>
-      )}
-    </div>
-  );
-};
+export const ToStorybook = () => (
+  <ModalUiDisplay>
+    {onClose => (
+      <Modal title="Modal title" onClose={onClose}>
+        <Text>Modal content</Text>
+      </Modal>
+    )}
+  </ModalUiDisplay>
+);
 
 ToStorybook.story = {
   name: "image-upload",
-  description: "description",
-  decorators: [(storyFn: () => ReactNode) => <Center size="large">{storyFn()}</Center>],
 };
