7f2982f56c14bf4e8547e78667fbac754a897d64
Daryl Browne <daryl.browne@googlemail.com>
2020-04-01 03:06:53 +0100

    chore(types): convienient types for common grommet types

---
+++ src/@types/props.ts
@@ -0,0 +1,4 @@
+import { BoxProps, LayerProps } from "grommet";
+
+export type GrommetBoxProps = BoxProps & JSX.IntrinsicElements["div"];
+export type GrommetLayerProps = LayerProps & JSX.IntrinsicElements["div"];
